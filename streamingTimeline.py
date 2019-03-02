837e54e8605942d7943e954940b0c415a18c514a
ShuDiamonds <fukudaubuntu@gmail.com>
2019-03-02 16:20:31 +0900

    reviewed code

--- streamingTimeline.py
+++ streamingTimeline.py
@@ -1,18 +1,32 @@
 import twitter
 import json
 
-rawfiletmp = open("account.json" , "r")
-accountjsonfile = json.load(rawfiletmp)
+SCREEN_NAME="KanNotice"
 
-auth = twitter.OAuth(consumer_key=accountjsonfile["consumer_key"],
-consumer_secret=accountjsonfile["consumer_secret"],
-token=accountjsonfile["token"],
-token_secret=accountjsonfile["token_secret"])
+if __name__ == '__main__':
+    rawfiletmp = open("account.json" , "r")
+    accountjsonfile = json.load(rawfiletmp)
 
-t = twitter.Twitter(auth=auth)
-#Userstreamを用いる
-t_userstream = twitter.TwitterStream(auth=auth,domain='userstream.twitter.com')
+    auth = twitter.OAuth(consumer_key=accountjsonfile["consumer_key"],
+    consumer_secret=accountjsonfile["consumer_secret"],
+    token=accountjsonfile["token"],
+    token_secret=accountjsonfile["token_secret"])
+    
+    """
+    t = twitter.Twitter(auth=auth)
+    #Userstreamを用いる
+    t_userstream = twitter.TwitterStream(auth=auth,domain='userstream.twitter.com')
 
-#自分のタイムラインのツイートおよびユーザーの情報が流れる
-for msg in twitter.userstream.user():
- print(msg)
+    #自分のタイムラインのツイートおよびユーザーの情報が流れる
+    for msg in twitter.userstream.user():
+        print(msg)
+    """
+    # Retrieve friends IDs
+    twitter_api = twitter.Twitter(auth=auth)
+    friends = twitter_api.friends.ids(screen_name=SCREEN_NAME, count=5000)
+    friends_ids = ','.join(map(str, friends['ids']))
+
+    stream = twitter.TwitterStream(auth=auth, secure=True)
+    for tweet in stream.statuses.filter(follow=friends_ids):
+        if 'user' in tweet and tweet['user']['id'] in friends['ids']:
+            print(tweet['user']['screen_name'], tweet['text'])
